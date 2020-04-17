ab2c18595007862a54d83e093235832172b4ea78
musawerfatih <63827449+musawerfatih@users.noreply.github.com>
2020-04-17 07:23:02 +0000

    Update and rename zeroboy.py to CLON.py

--- CLON.py
+++ CLON.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - FATIH -OFFICAL------•◈•
+|__\~~) •||•MUSAWER-FATIH---------------•◈•
+|__(-----\  •◈•------BLACK-EAGLE--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMusawer Fatih
+\033[1;96mInstagram \033[1;93m: \033[1;FlowDon't Have
+\033[1;96mFacebook  \033[1;93m: \033[1; zgujr
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/musawerfatih
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMusawer Fatih
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowDon't Have
+\033[1;96mFacebook  \033[1;93m: \033[1;92m zgujr
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/musawerfatih
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "zero"
+CorrectPassword = "cool"
 
 loop = 'true'
 while (loop == 'true'):
